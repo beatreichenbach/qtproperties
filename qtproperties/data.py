c35460b1dd94722cbbeac4494a18d9908d4ae6a4
Beat Reichenbach <info@artofbeat.com>
2022-09-28 00:31:50 -0700

    Alpha

--- qtproperties/data.py
+++ qtproperties/data.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+# TODO: implement operations
+
 
 @dataclass
 class Float2:
@@ -8,6 +10,31 @@ class Float2:
 
     def __iter__(self):
         return iter((self.x, self.y))
+
+    def __truediv__(self, other):
+        x = self.x.__truediv__(other.x)
+        y = self.y.__truediv__(other.y)
+        return self.__class__(x, y)
+
+    def __floordiv__(self, other):
+        x = self.x.__floordiv__(other.x)
+        y = self.y.__floordiv__(other.y)
+        return self.__class__(x, y)
+
+    def __add__(self, other):
+        x = self.x + other.x if isinstance(other, self.__class__) else self.x + other
+        y = self.y + other.y if isinstance(other, self.__class__) else self.y + other
+        return self.__class__(x, y)
+
+    def __sub__(self, other):
+        x = self.x - other.x if isinstance(other, self.__class__) else self.x - other
+        y = self.y - other.y if isinstance(other, self.__class__) else self.y - other
+        return self.__class__(x, y)
+
+    def __mul__(self, other):
+        x = self.x * other.x if isinstance(other, self.__class__) else self.x * other
+        y = self.y * other.y if isinstance(other, self.__class__) else self.y * other
+        return self.__class__(x, y)
 
 
 @dataclass
@@ -20,4 +47,29 @@ class Int2:
             setattr(self, attr, int(getattr(self, attr)))
 
     def __iter__(self):
-        return (self.x, self.y)
+        return iter((self.x, self.y))
+
+    def __truediv__(self, other):
+        x = self.x.__truediv__(other.x)
+        y = self.y.__truediv__(other.y)
+        return self.__class__(x, y)
+
+    def __floordiv__(self, other):
+        x = self.x.__floordiv__(other.x)
+        y = self.y.__floordiv__(other.y)
+        return self.__class__(x, y)
+
+    def __add__(self, other):
+        x = self.x + other.x if isinstance(other, self.__class__) else self.x + other
+        y = self.y + other.y if isinstance(other, self.__class__) else self.y + other
+        return self.__class__(x, y)
+
+    def __sub__(self, other):
+        x = self.x - other.x if isinstance(other, self.__class__) else self.x - other
+        y = self.y - other.y if isinstance(other, self.__class__) else self.y - other
+        return self.__class__(x, y)
+
+    def __mul__(self, other):
+        x = self.x * other.x if isinstance(other, self.__class__) else self.x * other
+        y = self.y * other.y if isinstance(other, self.__class__) else self.y * other
+        return self.__class__(x, y)
